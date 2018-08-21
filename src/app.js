d924937a4e64c060dabf7f5036d98cb0f2a18e06
王莹 <wangying@wangyingdeMBP.lan>
2018-08-21 23:17:47 +0800

    123123

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import "./index.scss"
+
 const fn =text=>console.log(text);
 
 fn('Hello World');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path=require('path');
 
 module.exports={
+    mode:"production",
     entry:"./src/app.js",
     output:{
         path:path.resolve(__dirname,"./dists"),
@@ -9,12 +10,16 @@ module.exports={
     module:{
         rules:[
             {
-                test:/\.js$/,
-                loader:"babel-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                  "babel-loader",
+                //   "eslint-loader",
+                ],
             },
             {
-                test:/\.scss/,
-                loader:['style-loader','css-loader','scss-loader']
+                test:/\.scss$/,
+                loader:['style-loader','css-loader','postcss-loader','sass-loader']
             }
         ]
     }
